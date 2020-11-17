47758ae19d7f47ee586c78b3f36ebe6692096782
Ludwika Malinowska <malinowska.L@wp.pl>
2020-11-17 18:54:27 +0100

    Zadanie 2 - test 1 - 1 to w systemie rzymskim I

---
+++ tests/Zadanie2_tests/RomanParameterizedPackage.py
@@ -0,0 +1,23 @@
+import unittest
+from parameterized import parameterized, parameterized_class
+
+from Zadanie2.Roman import RomanNumerals
+
+class RomanParameterizedPackage(unittest.TestCase):
+
+    def setUp(self):
+        self.tmp = RomanNumerals()
+
+    @parameterized.expand([
+        (1, "I")
+    ])
+
+    def test_one_parameterized(self,n, rom):
+        self.assertEqual(self.tmp.roman(n), rom)
+
+    def tearDown(self):
+        self.temp = None
+
+
+if __name__ == '__main__':
+    unittest.main()
