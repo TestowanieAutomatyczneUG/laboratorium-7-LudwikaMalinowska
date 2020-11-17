68f76eabd9dbe46d63e3af2f2452cdc6bf9d5ded
Ludwika Malinowska <malinowska.L@wp.pl>
2020-11-17 19:55:26 +0100

    Zadanie 2 - testy z pominieciem klas

--- tests/Zadanie2_tests/RomanParameterizedPackage.py
+++ tests/Zadanie2_tests/RomanParameterizedPackage.py
@@ -3,6 +3,9 @@ from parameterized import parameterized, parameterized_class
 
 from Zadanie2.Roman import RomanNumerals
 
+from nose.tools import assert_equal, assert_raises
+
+
 class RomanParameterizedPackage(unittest.TestCase):
 
     def setUp(self):
@@ -23,12 +26,55 @@ class RomanParameterizedPackage(unittest.TestCase):
         (1024, "MXXIV"),
         (3000, "MMM")
     ])
-
     def test_one_parameterized(self,n, rom):
         self.assertEqual(self.tmp.roman(n), rom)
 
     def tearDown(self):
         self.temp = None
+
+
+@parameterized([
+        (1, "I"),
+        (2, "II"),
+        (3, "III"),
+        (4, "IV"),
+        (6, "VI"),
+        (9, "IX"),
+        (27, "XXVII"),
+        (49, "XLIX"),
+        (59, "LIX"),
+        (141, "CXLI"),
+        (575, "DLXXV"),
+        (1024, "MXXIV"),
+        (3000, "MMM"),
+])
+def test_roman_outside_class(n, rom):
+    r = RomanNumerals()
+    assert_equal(r.roman(n), rom)
+
+
+@parameterized_class(('n', 'rom'), [
+        (1, "I"),
+        (2, "II"),
+        (3, "III"),
+        (4, "IV"),
+        (6, "VI"),
+        (9, "IX"),
+        (27, "XXVII"),
+        (49, "XLIX"),
+        (59, "LIX"),
+        (141, "CXLI"),
+        (575, "DLXXV"),
+        (1024, "MXXIV"),
+        (3000, "MMM"),
+])
+class RomanParameterizedPackage2(unittest.TestCase):
+    def setUp(self):
+        self.tmp = RomanNumerals()
+
+    def test_second_parameterized(self):
+        self.assertEqual(self.tmp.roman(self.n), self.rom)
+
 
 
 if __name__ == '__main__':
