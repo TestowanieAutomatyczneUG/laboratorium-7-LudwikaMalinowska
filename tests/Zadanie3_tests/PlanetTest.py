d10eeeead5cea8ec6b3199e7a4cc2d48112b62e7
Ludwika Malinowska <malinowska.L@wp.pl>
2020-11-17 20:15:45 +0100

    Zadanie 3 - dodano klase Planet i testy

---
+++ tests/Zadanie3_tests/PlanetTest.py
@@ -0,0 +1,43 @@
+import unittest
+from Zadanie3.Planet import Planet
+
+
+class PlanetTests(unittest.TestCase):
+
+    def setUp(self):
+        self.temp = Planet()
+
+    def test_mercury_age_correctly_counted(self):
+        self.assertEqual(self.temp.count_age_on_planet(2134835688, "Merkury"), 280.88)
+
+    def test_venus_age_correctly_counted(self):
+        self.assertEqual(self.temp.count_age_on_planet(1000000000, "Wenus"), 51.51)
+
+    def test_earth_age_correctly_counted(self):
+        self.assertEqual(self.temp.count_age_on_planet(1000000000.0, "Ziemia"), 31.69)
+
+    def test_mars_age_correctly_counted(self):
+        self.assertEqual(self.temp.count_age_on_planet(1000000000, "Mars"), 16.85)
+
+    def test_jupiter_age_correctly_counted(self):
+        self.assertEqual(self.temp.count_age_on_planet(1000000000, "Jowisz"), 2.67)
+
+    def test_saturn_age_correctly_counted(self):
+        self.assertEqual(self.temp.count_age_on_planet(1000000000, "Saturn"), 1.08)
+
+    def test_uranus_age_correctly_counted(self):
+        self.assertEqual(self.temp.count_age_on_planet(1000000000, "Uran"), 0.38)
+
+    def test_neptune_age_correctly_counted(self):
+        self.assertEqual(self.temp.count_age_on_planet(1000000000, "Neptun"), 0.19)
+
+
+    def test_mercury_exception1(self):
+        self.assertRaises(ValueError, self.temp.count_age_on_planet, 2134835688, "Merkur")
+
+    def test_mercury_exception2(self):
+        self.assertRaises(ValueError, self.temp.count_age_on_planet, "", "Merkury")
+
+    def test_mercury_exception3(self):
+        self.assertRaises(ValueError, self.temp.count_age_on_planet, "2134835688", "Merkury")
+
